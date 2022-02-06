e82f860f904a8b320a3c9a5f648aa1915159bcd5
lumix <over100stars@gmail.com>
2022-02-06 19:37:05 +0200

    added ;wordle

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,6 +2,7 @@ const { blue } = require("chalk");
 
 module.exports = (Discord, client, bot) => {
     client.emojiStreaks = new Discord.Collection();
+    client.games = {};
 
     console.log(blue("logged in as", client.user.tag));
 }
