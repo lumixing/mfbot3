48ce2ea5d4b9c11f11a125b869de9d737f702dac
lumix <over100stars@gmail.com>
2022-02-06 19:36:44 +0200

    added ;rw -c flag

--- commands/fun/randomword.js
+++ commands/fun/randomword.js
@@ -1,13 +1,21 @@
-const words = require("an-array-of-english-words")
+const words = require("an-array-of-english-words");
+const commonWords = require("../../assets/words.json").words;
+const { hasFlag } = require("../../util/util");
 
 module.exports = {
     async execute(Discord, client, msg, args, flags) {
         if (!isNaN(args[0])) {
-            const allowedWords = words.filter((w) => w.length === parseInt(args[0]));
+            let allowedWords = words.filter((w) => w.length === parseInt(args[0]));
 
             const rng = ~~(Math.random() * allowedWords.length);
             msg.reply(`(${rng}/${allowedWords.length}) ${allowedWords[rng]}`);
 
+            return;
+        }
+
+        if (hasFlag(flags, "c")) {
+            const rng = ~~(Math.random() * commonWords.length);
+            msg.reply(`(${rng}/${commonWords.length}) ${commonWords[rng]}`);
             return;
         }
 
@@ -17,7 +25,13 @@ module.exports = {
     name: "randomword",
     description: "gives a random word",
     aliases: ["rw"],
-    flags: [],
+    flags: [
+        {
+            key: "c",
+            description: "common words",
+            needsArgument: false
+        }
+    ],
     syntax: "randomword (length)",
     permissions: []
 }
